38b44388623c2f5a33c8a0fb3a11ebf442ea697c
Soma-Sundaram-sudo <sundaramsoma539@gmail.com>
2024-12-05 13:58:23 +0530

    new one

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () => {
         console.log("Database Connected");
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
+    await mongoose.connect("mongodb+srv://sundaramsoma539:8778354283@cluster0.jje7afv.mongodb.net/imagify?retryWrites=true&w=majority&appName=Cluster0")
 }
 
 export default connectDB
